38dfaae0cf9065508bb8f278ca7df3347d5bbcf0
zs <810909753@qq.com>
2018-06-17 10:30:23 +0800

    修改TITLE

--- tx_web/urls.py
+++ tx_web/urls.py
@@ -48,3 +48,5 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+    
